7d251c78045dc9f269d50fd3b86aaf5be11590fa
Anil Poudel <apoudel344@gmail.com>
2020-01-07 22:36:16 +0545

    angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+import { error } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngAll';
+  topics = [
+    'Angular', 'React', 'View'
+  ];
+  topicHasError = true;
+  userModel = new User('Rob' , 'Rob@gmail.com', 2735232735, 'default' , 'Morning', true);
+
+  constructor (private _enrollmentService: EnrollmentService) {}
+
+  validateTopic(value) {
+    if  (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false ;
+    }
+  }
+  onSubmit() {
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Success!', data) ,
+      error => console.error('Error', error)
+    )
+  }
+
+
 }
